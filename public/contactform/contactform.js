e516e5e34898458d5ffe740362cae93507d90395
BangMez <bangmez.bm@gmail.com>
2020-04-19 23:15:16 +0700

    Update index.html

--- public/contactform/contactform.js
+++ public/contactform/contactform.js
@@ -7,7 +7,8 @@ jQuery(document).ready(function($) {
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
+    f.children('input').each(function() { 
+	// run all inputs
 
       var i = $(this); // current input
       var rule = i.attr('data-rule');
